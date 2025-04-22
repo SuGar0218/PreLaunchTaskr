b7e84747367805278d122e88cde3179f503b0a61
SuGar0218 <wxk0218@icloud.com>
2025-04-22 20:40:49 +0800

    分离出WinUI3分支

--- PreLaunchTaskr.GUI.WPF/Helpers/DataContextHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace PreLaunchTaskr.GUI.WPF.Helpers;
-
-public class DataContextHelper
-{
-    public static T GetDataContext<T>(object frameworkElement) => (T) ((FrameworkElement) frameworkElement).DataContext;
-}
--- PreLaunchTaskr.GUI.WPF/Views/IProgramConfigCategoryPage.cs
+++
@@ -1,8 +0,0 @@
-﻿using PreLaunchTaskr.GUI.Common.AbstractViewModels.PageModels;
-
-namespace PreLaunchTaskr.GUI.WPF.Views;
-
-public interface IProgramConfigCategoryPage
-{
-    public IProgramConfigCategoryViewModel ViewModel { get; set; }
-}
