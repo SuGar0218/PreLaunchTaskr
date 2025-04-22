bf720a2e5086f4e03400607f4fe2d6862dfc8ee5
SuGar0218 <wxk0218@icloud.com>
2025-04-22 20:25:23 +0800

    分离出WPF分支

--- PreLaunchTaskr.GUI.WinUI3/Helpers/DataContextHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.UI.Xaml;
-
-namespace PreLaunchTaskr.GUI.WinUI3.Helpers;
-
-public class DataContextHelper
-{
-    public static T GetDataContext<T>(object frameworkElement) => (T) ((FrameworkElement) frameworkElement).DataContext;
-}
--- PreLaunchTaskr.GUI.WinUI3/Helpers/ForFilePicker/DirectorySavePickerHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.UI.Xaml;
-
-namespace PreLaunchTaskr.GUI.WinUI3.Helpers.ForFilePicker;
-
-public abstract class DirectorySavePickerHelper : DirectoryPickerHelper
-{
-    protected DirectorySavePickerHelper(Window window) : base(window) { }
-}
